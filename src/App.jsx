c74487801a150a50ad2c291e172d2fa0268bdf70
ak-adi <adisk.kumar.132@gmail.com>
2023-10-11 22:30:51 +0530

    adding reducers for auth services

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import { Footer } from './components'
+import {Header} from './components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+   //when app loads, we need to check if user logged in or not
+  //from state 
 
-  return (
-    <>
-    <h1>BLOGZILLA</h1>
-    </>
-  )
+  //first we need to create loading 
+  //when we fetch data from appwrite, so it takes some time to fetch for that we have to create loading
+  // using this we can do conditional rendering true-loading icon, false - no loading icon
+  //in stating we give default value true
+  // when application mounts, there loading status is true, then useEffect will work , in useEffect we false the loading
+  const[loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  //now when application loads, take useEffect, fir useEffect se phucho us service se ki aap loggedin ho ya nhi
+  useEffect(() => {
+    authService.getCurrentUser(login())
+    //jb current user mil gya
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        //if user not found, so calling activity logout so that state will update, that you are not logged in
+        dispatch(logout())
+      }
+    } )
+    .finally(() => setLoading(false))
+  },[])
+  //if loading false then fist statement execute other wise if true then null 
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
---
+++ src/store/authSlice.js
@@ -0,0 +1,33 @@
+//store for tracking authentication(user logged in or not)
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status : false,
+    userData : null
+}
+const authSlice = createSlice({
+    name :"auth",
+    initialState,
+    reducers: {
+        //login state
+        //action gives payload, state mai jo bhi value update krni hai wo initial state ke baad track mai update ho jati hai
+        login : (state,action) => {
+            //when login method dispatch, then make status true
+            state.status = true
+            state.userData = action.payload.userData
+        },
+        //don't need action payload
+        logout : (state) => {
+            state.status = false
+            state.userData = null
+        }
+    }
+})
+//reducers individual function, they also exported
+//authSlice ke andar reducers ke andar jo hai use actions bolte hai
+export const {login, logout} = authSlice.actions
+ 
+//exporting reducers
+export default authSlice.reducer
+
+//post bhi state mai hi jaane chaiye
