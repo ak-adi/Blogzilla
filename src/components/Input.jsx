6fcb4acd3e480ec499caef084a82c29d7b509034
ak-adi <adisk.kumar.132@gmail.com>
2023-10-12 21:54:44 +0530

    addition of form, editor in postfrom

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,7 @@
 import React, { useId } from 'react'
 
 //we define the input component in arrow function, for better understanding
+//we can write forwardref like this
 const Input = React.forwardRef(function Input({
     label,
     type = 'text',
@@ -12,11 +13,20 @@ const Input = React.forwardRef(function Input({
     return (
         <div className='w-full'>
             {/* if someone pass label then we display */}
-            {label && <label 
-            className='inline-block mb-1 pl-1' 
-            htmlFor={props.id}>
+            {label && <label
+                className='inline-block mb-1 pl-1'
+                // for accessibility purpose we use htmlfor id
+                htmlFor={id}>
                 {label}
-            </label> }
+            </label>}
+            <input type={type}
+            className={` px-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-ful ${className}`}
+            //this will give reference of parent component ,reference waha se pass kiya jayega aur yaha se fir state ka access liya jayega, then we get the values from input
+            ref={ref}           
+            {...props}
+            //now id is installed in label and here also, when clicked on that label, then cursor will point on that, and highlighted so we can write
+            id={id}
+             />
         </div>
     )
 })
